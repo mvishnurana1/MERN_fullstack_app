cac0014c5d9258055a56d17888659f3b89c72b0a
bulletTrain54 <mvishnurana@gmail.com>
2019-11-28 09:47:52 +0800

    UI plan added to different dir. Basic directory structure added. React DOM for route management of the SPA

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'; 
+import './App.css';
+
+import Users from './user/pages/Users'; 
+import NewPlaces from './places/pages/NewPlaces';  
+
+const App = () => {
+  return <Router>
+  <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+
+      <Route path="/places/new">
+        <NewPlaces />
+      </Route>
+
+      <Redirect to="/" />
+    </Switch>
+  </Router>; 
+}
+
+export default App;
---
+++ src/places/pages/NewPlaces.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPlaces = () => {
+    return (
+        <div>
+            <h1>New Places Work!</h1>
+        </div>
+    ); 
+}
+
+export default NewPlaces; 
